4e414b6e5cec4528479daa26e8aaf6ea69e543b1
lueduy <douglas20060530@gmail.com>
2025-08-07 22:38:07 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ls -al
+
+ffmpeg -re -stream_loop -1 -f concat -safe 0 -i playlist.txt \
+-loop 1 -i "直播的背景1.png" \
+-c:a aac -b:a 192k \
+-c:v libx264 -preset veryfast -tune stillimage \
+-pix_fmt yuv420p -shortest \
+-f flv "rtmp://a.rtmp.youtube.com/live2/733a-5hdz-gzf1-xjrz-5av6"
+
+sleep infinity
+
